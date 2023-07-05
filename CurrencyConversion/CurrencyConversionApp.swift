119d6aa18352a841e707f1cfd0e519c7c097233e
Muskan Jindal <muskanjindal78@gmail.com>
2023-07-05 13:33:48 +0530

    Initial Commit

---
+++ CurrencyConversion/CurrencyConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConversionApp.swift
+//  CurrencyConversion
+//
+//  Created by head_guest on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
